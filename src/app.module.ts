93e8d95264c311655a99d5e77c431e02c743bc65
“Mateusz” <mateuszludyk@gmail.com>
2023-11-09 13:37:56 +0100

    comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CommentModule],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
